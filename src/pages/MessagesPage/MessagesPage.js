87616f467bef0919ff7bcdbbb557043fe29e744d
ArnT81 <anders_soderberg@hotmail.com>
2023-12-19 14:37:51 +0100

    moved Error to correct folder & MessagesPage recieve messages where the current (mocked) user is either sender or reciever

--- src/pages/MessagesPage/MessagesPage.js
+++ src/pages/MessagesPage/MessagesPage.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Fetch } from '../../services/fetch';
-import Input from '../../components/Input/Input';
+import { Error } from '../../components/Error/Error';
 
+//TODO get all users the current user is chatting with
+//TODO present their name as links with route to conversation
+//TODO in new link make it possible to answere to the other user
+//TODO service that handles the timestamp
+//TODO a function that sends a request and get only messages between user and recipient  
 
 export default function MessagesPage() {
 	const [messages, setMessages] = useState() /* <= state (values ment to update the component when changed)*/
 	const [error, setError] = useState()
+	const temporaryId = 1; //TODO should come from url after login
 
 	useEffect(() => {
 		getUsersMessages()
 	}, []) // <= dependency array (only happen once if empty)
 
-	const getUsersMessages = () => {
-		Fetch('messages/?userId=1')
+
+	const getUsersMessages = () => {	//Where current user is either "userId" or "recipient"
+		Fetch(`messages?userId=${temporaryId}`)
 			.then(res => {
-				console.log(res);
 				if (res?.error) setError(res.error);
 				else setMessages(res)
 			})
@@ -22,16 +28,40 @@ export default function MessagesPage() {
 			.catch(err => console.log(err))
 	}
 
+
 	return (
 		<div>
-			<Input
-				type="text"
-				placeholder="first name"
-			/>
-			<Input
-				type="text"
-				placeholder="last name"
-			/>
+			<h2>Messages</h2>
+			<p>messages where current user is recipient are displayed on the right instead</p>
+			<br />
+
+			{error && <Error error={error} />}
+
+			{messages &&
+				<div>
+					{messages.map((message) => {
+						return (
+							<div
+								key={message.id}
+								style={{
+									display: 'flex',
+									padding: 'var(--spacing)',
+									gap: 'var(--spacing)',
+									marginBottom: '10px',
+									border: '1px solid blue',
+									justifyContent: message.recipient === temporaryId ? 'end' : 'start'
+								}}
+							>
+								<p>id: {message.id}</p>
+								<p>created: {message.created}</p>
+								<p>userId: {message.userId}</p>
+								<p>recipient: {message.recipient}</p>
+								<p>content: {message.content}</p>
+							</div>
+						)
+					})}
+				</div>
+			}
 		</div>
 	)
 }
